49d62085c7cf5cba1d9e22d6d75fd95ddb79e5d8
Matthew Berryhill <matthewberryhill@gmail.com>
2018-11-28 17:58:16 -0700

    https://github.com/pineapplemidi/pwlaunch/issues/1 create session box from config

---
+++ PwLaunch.py
@@ -0,0 +1,43 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import Live
+import io
+import json
+import sys
+
+sys.path.append('/Library/Frameworks/Python.framework/Versions/2.7/ \
+lib/python2.7/site-packages/')
+
+from _Framework.ControlSurface import ControlSurface
+from _Framework.SessionComponent import SessionComponent
+from _Framework.ButtonElement import ButtonElement
+
+
+class PwLaunch(ControlSurface):
+
+    def __init__(self, c_instance):
+        super(PwLaunch, self).__init__(c_instance)
+        with self.component_guard():
+            is_momentary = True
+            config = self._open_config()
+            self._session = self._setup_session(config['width'], config['height'])
+            self.set_highlighting_session_component(self._session)
+
+    def _open_config(self):
+        with open('/users/berry/config.json') as f:
+            data = json.load(f)
+            self.log(json.dumps(data))
+            return json.loads(json.dumps(data))
+
+    def _setup_session(self, width, height):
+        session = SessionComponent(width, height)
+        session.set_offsets(0, 0)
+        return session
+
+    def log(self, message):
+        log_str = 'LOG: ' + message + '\n'
+        with io.open(
+            "/users/berry/somefile.txt",
+                mode='w', encoding='utf-8') as f:
+            f.write(log_str)
+            f.close()
